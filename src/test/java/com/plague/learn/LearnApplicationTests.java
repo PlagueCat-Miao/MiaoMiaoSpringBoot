fe6d6ab9f92ec7e615235fbec327d966762e8ade
PlagueCat-Miao <ace0413@126.com>
2022-02-03 20:11:16 +0800

    硬核回退

--- src/test/java/com/plague/learn/LearnApplicationTests.java
+++ src/test/java/com/plague/learn/LearnApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo.smm;
+package com.plague.learn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootMysqlMybatisDemoApplicationTests {
+class LearnApplicationTests {
 
     @Test
     void contextLoads() {
